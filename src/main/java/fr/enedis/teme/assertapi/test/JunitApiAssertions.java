101e8dd3a768383bc5536910cd334f701499e42f
uSf <youssef.alami-harchali@enedis.fr>
2021-10-25 22:30:00 +0200

    edit

---
+++ src/main/java/fr/enedis/teme/assertapi/test/JunitApiAssertions.java
@@ -0,0 +1,61 @@
+package fr.enedis.teme.assertapi.test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import org.skyscreamer.jsonassert.JSONCompareResult;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientResponseException;
+
+import fr.enedis.teme.assertapi.core.ResponseComparator;
+
+public final class JunitApiAssertions implements ResponseComparator {
+	
+	@Override
+	public void assumeEnabled(boolean enable) {
+		assumeTrue(enable, "skipped test");
+	}
+	
+	@Override
+	public ResponseEntity<byte[]> assertNotResponseException(SafeSupplier<ResponseEntity<byte[]>> supp) {
+		return assertDoesNotThrow(supp::get, "error");
+	}
+
+	@Override
+	public RestClientResponseException assertResponseException(SafeSupplier<?> supp) {
+		return assertThrows(RestClientResponseException.class, supp::get, "error");
+	}
+
+	@Override
+	public void assertStatusCode(int expectedStatusCode, int actualStatusCode) {
+		assertEquals(expectedStatusCode, actualStatusCode, "Status code");
+	}
+
+	@Override
+	public void assertContentType(MediaType expectedContentType, MediaType actualContentType) {
+		assertEquals(expectedContentType, actualContentType, "Content Type");
+	}
+	
+	@Override
+	public void assertByteContent(byte[] expectedContent, byte[] actualContent) {
+		assertArrayEquals(expectedContent, actualContent, "Response content");
+	}
+	
+	@Override
+	public void assertTextContent(String expectedContent, String actualContent) {
+    	assertEquals(expectedContent, actualContent, "Response content");
+	}
+
+	@Override
+	public void assertJsonCompareResut(JSONCompareResult res) {
+		if (res.failed()) {
+            throw new AssertionError(res.getMessage());
+        }
+	}
+
+
+}
