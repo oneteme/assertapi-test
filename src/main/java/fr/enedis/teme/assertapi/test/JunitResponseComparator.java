cdf25cf993a59569fd53a332b59713e2104a7bf0
uSf <youssef.alami-harchali@enedis.fr>
2021-11-05 10:08:03 +0100

    edit

--- src/main/java/fr/enedis/teme/assertapi/test/JunitResponseComparator.java
+++ src/main/java/fr/enedis/teme/assertapi/test/JunitResponseComparator.java
@@ -1,15 +1,11 @@
 package fr.enedis.teme.assertapi.test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import org.skyscreamer.jsonassert.JSONCompareResult;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestClientResponseException;
 
 import fr.enedis.teme.assertapi.core.ResponseComparator;
 
@@ -18,16 +14,6 @@ public final class JunitResponseComparator implements ResponseComparator {
 	@Override
 	public void assumeEnabled(boolean enable) {
 		assumeTrue(enable, "skipped test");
-	}
-	
-	@Override
-	public ResponseEntity<byte[]> assertNotResponseException(SafeSupplier<ResponseEntity<byte[]>> supp) {
-		return assertDoesNotThrow(supp::get, "Actual response exception");
-	}
-
-	@Override
-	public RestClientResponseException assertResponseException(SafeSupplier<?> supp) {
-		return assertThrows(RestClientResponseException.class, supp::get, "Expected response exception");
 	}
 
 	@Override
@@ -55,5 +41,10 @@ public final class JunitResponseComparator implements ResponseComparator {
 		if (res.failed()) {
             throw new AssertionError(res.getMessage());
         }
+	}
+	
+	@Override
+	public void assertionFail(Throwable t) {
+		throw new AssertionError("Assertion fail", t);
 	}
 }
