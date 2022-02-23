bbbd9a7bafec5451089ea31196e679389ade5b91
uSf <youssef.alami-harchali@enedis.fr>
2022-02-23 15:57:35 +0100

    edit

--- src/main/java/fr/enedis/teme/assertapi/test/TestCaseProvider.java
+++ src/main/java/fr/enedis/teme/assertapi/test/TestCaseProvider.java
@@ -9,11 +9,15 @@ import java.net.URI;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
-import fr.enedis.teme.assertapi.core.HttpQuery;
+import fr.enedis.teme.assertapi.core.ApiRequest;
 import fr.enedis.teme.assertapi.core.ServerConfig;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -21,32 +25,32 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public final class TestCaseProvider {
 	
-	public static final Stream<HttpQuery> fromRepository(String url){
+	public static final Stream<ApiRequest> fromRepository(String url){
 		
 		return fromRepository(url, null);
 	}
 	
-	public static final Stream<HttpQuery> fromRepository(String uri, ServerConfig config){
+	public static final Stream<ApiRequest> fromRepository(String uri, ServerConfig config){
 		
 		var template = config == null ? new RestTemplate() : build(config);
-		return Stream.of(template.getForObject(uri, HttpQuery[].class));
+		injectMapper(template);
+		return Stream.of(template.getForObject(uri, ApiRequest[].class));
 	}
 	
-	public static Stream<HttpQuery> jsonRessources(URI uri) {
+	public static Stream<ApiRequest> jsonRessources(URI uri) {
 		
 		return jsonRessources(uri, p-> p.getName().toLowerCase().endsWith(".json"));
 	}
 	
-	public static Stream<HttpQuery> jsonRessources(URI uri, String filenamePattern) {
+	public static Stream<ApiRequest> jsonRessources(URI uri, String filenamePattern) {
 		
 		return jsonRessources(uri, filenamePattern == null ? null : p-> p.getName().matches(filenamePattern));
 	}
 
-	public static Stream<HttpQuery> jsonRessources(URI uri, Predicate<File> predicate) {
-		var mapper = new ObjectMapper();
+	public static Stream<ApiRequest> jsonRessources(URI uri, Predicate<File> predicate) {
 		return searchIn(uri, predicate).flatMap(f-> {
 			try {
-				return Stream.of(mapper.readValue(f, HttpQuery[].class));
+				return Stream.of(mapper().readValue(f, ApiRequest[].class));
 			} catch (IOException e) {
 				throw new IllegalArgumentException("Canot parse file " + f.getName(), e);
 			}
@@ -65,4 +69,19 @@ public final class TestCaseProvider {
 		}
 		return Stream.of(f.listFiles()).filter(filter);
 	}
+
+	private static void injectMapper(RestTemplate template) {
+
+        for(HttpMessageConverter<?> mc : template.getMessageConverters()) {
+        	if(mc instanceof MappingJackson2HttpMessageConverter) {
+        		((MappingJackson2HttpMessageConverter)mc).setObjectMapper(mapper());
+        	}
+        }
+	}
+	
+	private static ObjectMapper mapper() {
+		return Jackson2ObjectMapperBuilder.json().build()
+				.registerModule(new ParameterNamesModule());
+	}
+	
 }
