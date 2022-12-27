6aceda228071193ed15fb462df3a32c56376c0c5
u$f <usf.alami@gmail.com>
2022-12-28 00:38:20 +0100

    edit

--- src/main/java/org/usf/assertapi/test/JunitResponseComparator.java
+++ src/main/java/org/usf/assertapi/test/JunitResponseComparator.java
@@ -1,8 +1,9 @@
 package org.usf.assertapi.test;
 
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
-
+import org.opentest4j.AssertionFailedError;
+import org.opentest4j.TestAbortedException;
 import org.usf.assertapi.core.ApiAssertionError;
+import org.usf.assertapi.core.CompareStage;
 import org.usf.assertapi.core.ResponseComparator;
 
 /**
@@ -16,11 +17,22 @@ public final class JunitResponseComparator extends ResponseComparator {
 	@Override
 	public void assumeEnabled(boolean enabled) {
 		try {
-			super.assumeEnabled(enabled); //log error
+			super.assumeEnabled(enabled); //super log error
 		}
 		catch(ApiAssertionError e) {
-			assumeTrue(false, "api assertion skipped"); //specific JUnit exception for skipping test
+			//throw the right junit exception (IDE plugin comparator)
+			throw new TestAbortedException("api assertion skipped"); 
 		}
 	}
 	
+	@Override
+	protected void failNotEqual(Object expected, Object actual, CompareStage stage) {
+		try {
+			super.failNotEqual(expected, actual, stage); //super log error
+		}
+		catch(ApiAssertionError e) {
+			//throw the right junit exception (IDE plugin comparator)
+			throw new AssertionFailedError(e.getMessage(), e.getExpected(), e.getActual()); 
+		}
+	}
 }
