8c3354f778f2481a1a720c8c6629aa8de698b235
u$f <usf.alami@gmail.com>
2023-02-03 19:54:07 +0100

    edit

--- src/main/java/org/usf/assertapi/test/TestCaseProviderBuilder.java
+++ src/main/java/org/usf/assertapi/test/TestCaseProviderBuilder.java
@@ -1,29 +1,38 @@
 package org.usf.assertapi.test;
 
+import static org.usf.assertapi.core.PolymorphicType.typeName;
 import static org.usf.assertapi.core.Utils.defaultMapper;
 
-import org.usf.assertapi.core.DataComparator;
-import org.usf.assertapi.core.DataTransformer;
+import org.usf.assertapi.core.PolymorphicType;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.NamedType;
 
+import lombok.AccessLevel;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public final class TestCaseProviderBuilder {
 	
 	final ObjectMapper mapper;
 	
 	public TestCaseProviderBuilder() {
-		this.mapper = defaultMapper();
-	}
-	
-	public TestCaseProviderBuilder registerComaparator(@NonNull String name, @NonNull Class<? extends DataComparator<?>> c) {
-		mapper.registerSubtypes(new NamedType(c, name));
-		return this;
+		this(defaultMapper());
 	}
 
-	public TestCaseProviderBuilder registerTransformer(@NonNull String name, @NonNull Class<? extends DataTransformer<?,?>> c) {
+	@SuppressWarnings("unchecked")
+	public TestCaseProviderBuilder register(@NonNull Class<? extends PolymorphicType>... classes) {
+		for(var c : classes) {
+			if(typeName(c) == null) {
+				throw new IllegalArgumentException("cannot find annotation @JsonTypeName on " + c.getCanonicalName());
+			}
+		}
+		mapper.registerSubtypes(classes);
+		return this;
+	}
+	
+	public TestCaseProviderBuilder register(@NonNull String name, @NonNull Class<? extends PolymorphicType> c) {
 		mapper.registerSubtypes(new NamedType(c, name));
 		return this;
 	}
@@ -31,5 +40,4 @@ public final class TestCaseProviderBuilder {
 	public TestCaseProvider build() {
 		return new TestCaseProvider(mapper);
 	}
-
 }
